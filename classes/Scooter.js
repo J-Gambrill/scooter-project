f23a2f4f00aeb023ff2fe3dfbe76d46da4f542cd
James Gambrill <jameswgambrill@icloud.com>
2024-10-27 17:17:40 +0000

    not sure how this is going to work yet just wanted to add the first commit

--- classes/Scooter.js
+++ classes/Scooter.js
@@ -1,5 +1,14 @@
-class Scooter {
-  // Scooter code here
+const User = require("./User");
+
+class Scooter extends User {
+
+  constructor(station, user, serial, nextSerial = 1, charge = 100, isBroken = false){
+    user = super(username)
+    this.station = station
+    nextSerial = serial
+    this.charge = charge
+    this.isBroken = isBroken
+  }
 }
 
 module.exports = Scooter;
--- classes/User.js
+++ classes/User.js
@@ -1,5 +1,24 @@
 class User {
-  // User code here
+  constructor(username, password, age, loggedIn = false){
+    this.username = username
+    this.password = password
+    this.age = age
+    this.loggedIn = loggedIn
+  }
+
+  logIn(password){
+    if (password === this.password) {
+      this.loggedIn = true
+    } else {
+      throw new Error('incorrect password')
+    }
+  }
+
+  logOut(){
+    this.loggedIn = false
+  }
+
+
 }
 
 module.exports = User;
